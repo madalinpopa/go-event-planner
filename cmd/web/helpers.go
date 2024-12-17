f875a314d2ca2477c802ea0fe73c9a1ad29eae2a
Madalin Popa <contact@madalinpopa.com>
2024-12-17 20:58:07 +0100

    Add error handling helpers for client and server responses

---
+++ cmd/web/helpers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"runtime/debug"
+)
+
+// serverError logs an internal server error and sends a 500 status response with a generic error message to the client.
+func (app *App) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	var (
+		method = r.Method
+		url    = r.URL.RequestURI()
+		trace  = string(debug.Stack())
+	)
+
+	app.logger.Error(err.Error(), "method", method, "url", url, "trace", trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// clientError logs client-side errors and sends the corresponding HTTP status code and message to the client.
+func (app *App) clientError(w http.ResponseWriter, r *http.Request, status int, err error) {
+	var (
+		method = r.Method
+		url    = r.URL.RequestURI()
+	)
+	app.logger.Error(err.Error(), "method", method, "url", url, "status", status)
+	http.Error(w, http.StatusText(status), status)
+}
