a3f56bcb9f322458199155a8a63cf1a3f5626bd6
Madalin Popa <contact@madalinpopa.com>
2024-12-20 22:14:31 +0100

    Simplify User model by merging password fields

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,11 +4,10 @@ import "database/sql"
 
 // User represents a user entity with basic identification and authentication fields.
 type User struct {
-	ID        int
-	Name      string
-	Email     string
-	Password1 string
-	Password2 string
+	ID       int
+	Name     string
+	Email    string
+	Password string
 }
 
 // UserModel provides methods to interact with the users' data in the database using an sql.DB instance.
@@ -16,11 +15,11 @@ type UserModel struct {
 	DB *sql.DB
 }
 
-func (m *UserModel) Create(name, email, password1, password2 string) error {
+func (m *UserModel) Create(name, email, password string) error {
 	_ = name
 	_ = email
-	_ = password1
-	_ = password2
+	_ = password
+
 	return nil
 }
 
