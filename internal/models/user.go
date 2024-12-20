9810e4f897fe7961e63269625685d2407ae5ea28
Madalin Popa <contact@madalinpopa.com>
2024-12-20 22:22:12 +0100

    Update User model with hashed password and new methods

--- internal/models/user.go
+++ internal/models/user.go
@@ -2,28 +2,35 @@ package models
 
 import "database/sql"
 
-// User represents a user entity with basic identification and authentication fields.
+// User represents a user entity with basic
+// identification and authentication fields.
 type User struct {
-	ID       int
-	Name     string
-	Email    string
-	Password string
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
 }
 
-// UserModel provides methods to interact with the users' data in the database using an sql.DB instance.
+// UserModel provides methods to interact with the users'
+// data in the database using an sql.DB instance.
 type UserModel struct {
 	DB *sql.DB
 }
 
-func (m *UserModel) Create(name, email, password string) error {
-	_ = name
-	_ = email
-	_ = password
-
+// Create adds a new user with the provided name,
+// email, and hashed password to the database.
+func (m *UserModel) Create(name, email string, password []byte) error {
 	return nil
 }
 
-func (m *UserModel) Get(id int) (User, error) {
-	_ = id
-	return User{}, nil
+// Authenticate verifies a user's credentials and returns
+// their ID if valid or an error if authentication fails.
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	return 0, nil
+}
+
+// Exists checks if a user with the specified ID exists in the
+// database and returns a boolean result and an error.
+func (m *UserModel) Exists(id int) (bool, error) {
+	return false, nil
 }
