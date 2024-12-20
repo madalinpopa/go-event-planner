4fe2e990b11d77d8ded197af39bb978d8b6ae3ba
Madalin Popa <contact@madalinpopa.com>
2024-12-20 22:47:20 +0100

    Add EventForm and UserRegisterForm structs for form handling

--- cmd/web/forms.go
+++ cmd/web/forms.go
@@ -5,10 +5,19 @@ import (
 	"time"
 )
 
+// EventForm represents a data structure for handling event creation form input and validation.
 type EventForm struct {
 	Title               string    `form:"title"`
 	Description         string    `form:"description"`
 	Location            string    `form:"location"`
 	EventDate           time.Time `form:"eventDate"`
+	validator.Validator `form:"-"`
+}
+
+// UserRegisterForm represents the structure for a user registration form containing name, email, and password fields.
+type UserRegisterForm struct {
+	Name                string `form:"name"`
+	Email               string `form:"email"`
+	Password            string `form:"password"`
 	validator.Validator `form:"-"`
 }
