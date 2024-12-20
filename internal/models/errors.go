cf3ca654754d0653cae210b7408007d2e717e327
Madalin Popa <contact@madalinpopa.com>
2024-12-20 22:22:07 +0100

    Add new error variables for invalid credentials and duplicate email

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -2,5 +2,16 @@ package models
 
 import "errors"
 
-// ErrNoRecord indicates that no matching record was found in the database or data source.
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+
+	// ErrNoRecord indicates that no matching record was found in the database or data source.
+	ErrNoRecord = errors.New("models: no matching record found")
+
+	// ErrInvalidCredentials indicates that the provided user credentials are
+	// invalid or do not match any record in the system.
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+
+	// ErrDuplicateEmail indicates that the provided email already exists
+	// in the system and cannot be used again.
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
