7033514ad2f967ec21b47207755c9f36d191fbd5
Madalin Popa <contact@madalinpopa.com>
2024-12-23 11:41:47 +0100

    Update route URL for event detail endpoint

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,7 +23,7 @@ func (app *App) routes() http.Handler {
 	// Public routes
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /ping", dynamic.ThenFunc(app.ping))
-	mux.Handle("GET /events/detail/{id}", dynamic.ThenFunc(app.eventView))
+	mux.Handle("GET /events/{id}", dynamic.ThenFunc(app.eventView))
 	mux.Handle("GET /events", dynamic.ThenFunc(app.eventList))
 
 	// Protected routes
